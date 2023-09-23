a01ce7d2c8c7fa16374e0821b36963f39c17eb29
Marcus Lee <marcustutorial@hotmail.com>
2023-09-23 10:41:32 +0100

    feat: added CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+## v0.1.2 (2023-09-23)
+
+*  feat: use DateTime<Utc> instead of NaiveDate [View](https://github.com/cybotrade/tardis-rs/commits/06859b0ebac1d9400463def54917c553a0050e83)
+
+
+## v0.1.1 (2023-09-22)
+
+*  fix: exposed fields in Trade, BookChange, DerivativeTicker to be public [View](https://github.com/cybotrade/tardis-rs/commits/eba040c67217562aa24c5235357b86fd8d60dced)
+*  feat: added a shell script for generating changelog [View](https://github.com/cybotrade/tardis-rs/commits/dc46434fe3c0264fd91a273e23ea732f47426f0e)
+
+
--- changelog.sh
+++ changelog.sh
@@ -6,7 +6,7 @@ do
 if [ "$previous_tag" != 0 ];then
     tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${previous_tag})
     printf "## ${previous_tag} (${tag_date})\n\n"
-    git log ${current_tag}...${previous_tag} --pretty=format:'*  %s [View](https://bitbucket.org/projects/test/repos/my-project/commits/%H)' --reverse | grep -v Merge
+    git log ${current_tag}...${previous_tag} --pretty=format:'*  %s [View](https://github.com/cybotrade/tardis-rs/commits/%H)' --reverse | grep -v Merge
     printf "\n\n"
 fi
 previous_tag=${current_tag}
